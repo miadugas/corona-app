f7cd3a1709cebfbf46124cabddd07eb26932a445
Mia Dugas <miadugas@outlook.com>
2020-04-05 18:59:04 -0600

    corona-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+render() {
+return (
+    <div>
+        <h1>
+            App
+        </h1>
+    </div>
+)
+
+
+}
+}
+
+export default App;
