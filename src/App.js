1fff4bac18c0ed783abbeb1d3d3cebb36c493abf
Mia Dugas <miadugas@outlook.com>
2020-04-05 19:59:23 -0600

    corona-app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
+//class based because its easier 
 class App extends React.Component {
+
+async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+}
+
+
+
+
 render() {
 return (
     <div className={styles.container}>
