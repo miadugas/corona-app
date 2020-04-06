6de681ef78e0264a1be529eb9ce2d5ae0f413bcf
Mia Dugas <miadugas@outlook.com>
2020-04-06 00:36:53 -0600

    corona-app

---
+++ build/precache-manifest.45ce72e8c208c402e4e97850b1cf1b79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba78509780f331886c4db78972dbde36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f6dab1697376f745b8",
+    "url": "/static/css/main.239108a1.chunk.css"
+  },
+  {
+    "revision": "239f337c33a73cdd97c7",
+    "url": "/static/js/2.b6662921.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/static/js/2.b6662921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f6dab1697376f745b8",
+    "url": "/static/js/main.ce833ca3.chunk.js"
+  },
+  {
+    "revision": "5f8578dc35b1032d0e12",
+    "url": "/static/js/runtime-main.272ede38.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
